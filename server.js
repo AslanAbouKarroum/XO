868cb3a49f815760acf2c6225f4ca0ee6cf0167b
AslanAbouKarroum <aslaneak153@gmail.com>
2025-03-14 15:31:56 +0200

    changing the port to 3000 and cmd to server.js in the dockerfilr

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 require('dotenv').config();
-const PORT = 1990;
+const PORT = 3000;
 
 const app = express();
 
